1f5c0179be93b7c18dfde245a106b7362244c92b
Gopesh Janardhanan <gopeshjana@gmail.com>
2024-05-17 22:29:04 +0400

    Add timer on Navbar
    edited timer.js and navbar.html
    added timer countdown from 2hrs

---
+++ static/js/timer.js
@@ -0,0 +1,27 @@
+let hrs_el = document.querySelector('#hrs')
+let mins_el = document.querySelector('#mins')
+let secs_el = document.querySelector('#secs')
+function timeleft() {
+    let date = new Date()
+    const starttime = 20*3600
+    let seconds = date.getSeconds()
+    let mins = date.getMinutes() *60
+    let hours = date.getHours() *3600
+    let left0= (seconds+mins+hours) - starttime
+    let left = 7200 - left0
+    let hrs = Math.floor(left / 3600)
+    let mins1 = Math.floor((left-(hrs*3600) )/60)
+    let secs = Math.floor((left -(hrs * 3600 + mins1*60) ))
+    if (mins1<10){
+        mins1 = '0' + mins1
+    }if (secs<10){
+        secs = '0' + secs
+    }
+    hrs_el.textContent = '0'+hrs + ":"
+    mins_el.textContent = mins1 + ":"
+    secs_el.textContent = secs
+    // console.log( [hrs,mins1,secs])
+}
+
+
+setInterval(timeleft,100)
