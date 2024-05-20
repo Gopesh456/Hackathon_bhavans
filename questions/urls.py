e3ce0820fa082fefb4329babcc30f9d0fff434dd
Gopesh Janardhanan <gopeshjana@gmail.com>
2024-05-21 00:10:44 +0400

    client side update v1.1
    -Client side answer, results saves on the db.
    -bug: fix points, remove unwanted code

--- questions/urls.py
+++ questions/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('questions/',views.overview,name = "questions"),
     path('getpoints/',views.getPoints,name = "getpoints"),
     path('getresults/',views.getResults,name = "getresults"),
+    path('post_correct/',views.getCorrect,name = "post_correct"), # type: ignore
 ]
