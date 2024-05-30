da03b4800432950834a4ad9493b0693edf5f093d
Gopesh Janardhanan <gopeshjana@gmail.com>
2024-05-30 22:39:08 +0400

    deleted unwanted files

--- user/consumers.py
+++
@@ -1,8 +0,0 @@
-import json
-from channels.generic.websocket import WebsocketConsumer
-
-
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
-        self.send(text_data=json.dumps({'message': 'Hello World'}))
